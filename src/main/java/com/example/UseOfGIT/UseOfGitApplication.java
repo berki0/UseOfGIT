f697f1beec9682d1e3f6cbe6de4ffb4b966683bb
berki0 <berki9@icloud.com>
2023-05-09 21:32:18 +0300

    Initial commit

---
+++ src/main/java/com/example/UseOfGIT/UseOfGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.UseOfGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseOfGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseOfGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UseOfGIT/UseOfGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UseOfGIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseOfGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
