c90184312064e506650187727da415d97f0c442f
berki0 <berki9@icloud.com>
2023-05-09 21:35:51 +0300

    creating entity

---
+++ src/main/java/com/example/UseOfGIT/User.java
@@ -0,0 +1,19 @@
+package com.example.UseOfGIT;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String userName;
+    private String password;
+
+
+}
